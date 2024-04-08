3759cd6ed70d48cba1730977ee7cb76ea2270410
林张生 <linzhangsheng23@gmail.com>
2024-04-08 17:56:38 +0800

    feat: add auth ui

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,5 +3,24 @@ export default function AuthLayout({
 }: {
   children: React.ReactNode
 }) {
-  return <div>{children}</div>
+  return (
+    <div className="flex h-full">
+      <div className="flex h-full w-full flex-col border-r lg:w-2/5">
+        <div className="p-6 text-xl font-semibold">Dev Rocket</div>
+        <div className="mx-auto flex-1 pt-10">{children}</div>
+        <div className="p-6 text-center text-xs text-muted-foreground">
+          By continuing, you agree to{' '}
+          <span className="cursor-pointer underline hover:opacity-75">
+            Terms of Service
+          </span>{' '}
+          and{' '}
+          <span className="cursor-pointer underline hover:opacity-75">
+            Privacy Policy
+          </span>
+          .
+        </div>
+      </div>
+      <div className="hidden h-full lg:block lg:flex-1">right</div>
+    </div>
+  )
 }
--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,3 +1,67 @@
+import Link from 'next/link'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { GithubLogo } from '@/components/github-logo'
+import { GoogleLogo } from '@/components/google-logo'
+
 export default function Page() {
-  return <div>Sign In</div>
+  return (
+    <div className="flex w-[400px] flex-col gap-y-8">
+      <div>
+        <h1 className="text-2xl font-semibold">Welcome back</h1>
+        <p className="text-muted-foreground">Sign in to your account</p>
+      </div>
+      <div className="space-y-4">
+        <Button className="w-full" variant="secondary">
+          <GithubLogo className="mr-2" />
+          Continue with Github
+        </Button>
+        <Button className="w-full" variant="outline">
+          <GoogleLogo className="mr-2" />
+          Continue with Google
+        </Button>
+      </div>
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <div className="w-full border-t"></div>
+        </div>
+        <div className="relative flex justify-center text-sm">
+          <span className="bg-white px-2 text-sm text-muted-foreground dark:bg-black">
+            or
+          </span>
+        </div>
+      </div>
+      <div className="grid gap-4">
+        <div className="grid gap-2">
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" type="email" placeholder="m@example.com" required />
+        </div>
+        <div className="grid gap-2">
+          <div className="flex items-center">
+            <Label htmlFor="password">Password</Label>
+            <Link href="#" className="ml-auto inline-block text-sm underline">
+              Forgot your password?
+            </Link>
+          </div>
+          <Input
+            id="password"
+            type="password"
+            placeholder="********"
+            required
+          />
+        </div>
+        <Button type="submit" className="w-full">
+          Sign in
+        </Button>
+      </div>
+      <div className="mt-4 text-center text-sm">
+        Don&apos;t have an account?{' '}
+        <Link href="/sign-up" className="underline hover:opacity-75">
+          Sign Up Now
+        </Link>
+      </div>
+    </div>
+  )
 }
